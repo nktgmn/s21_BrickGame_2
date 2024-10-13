d7c8256c058117152da2ddd3e3acbc773af9869b
gafych <gafych@gmail.com>
2024-10-13 12:28:56 +0300

    desktop done

--- src/build/CMakeFiles/BrickGame.dir/game_params.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/BrickGame.dir/game_params.cpp.o: \
-  /Users/nikita/Projects/GitHub/s21_BrickGame_2/src/game_params.cpp \
-  /Users/nikita/Projects/GitHub/s21_BrickGame_2/src/game_params.h
